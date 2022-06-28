4b20b39ff7d332bbacb7f91bd20a4e8aaa6e2ce8
Kurt Lourens <kurt.lourens@outlook.com>
2022-06-28 20:05:46 +0200

    new testimonials

--- assistantnms_web/assets/js/testimonials.js
+++ assistantnms_web/assets/js/testimonials.js
@@ -2,98 +2,98 @@ var testimonialList = [
 
 {
 guid: 'a1d760e9-4475-4705-a101-e15aa9ecefc1',
-name: 'Amarianna4356',
-review: `Awesome... and 100% FREE!!!! Love this app has great tips and ingredient lists for craft-ables and refined materials. As well as an amazing support staff who really care about the game and the players who use their app. They keep it updated with the newest patches. And they also have a discord server where you can ask for help and/or bring up anything you may notice on the app that has not been perfected yet. Would definitely recommend for all level players of No Mans Sky, especially the new players who don’t know the mechanisms yet. Thank You!!!`,
-source: '2',
-imageUrl: 'https://nmsassistant.com/assets/img/AppStoreIcon.png'
+name: 'Bud Green',
+review: `Thank you, fellow Traveller. This app makes so much of the game more understandable, organizational, and is the perfect companion to keep track of where you are at. No Man's Sky is so huge with so many moving parts, and it's constantly changing. This compendium guide is a must have by anyone who likes the game, even casually. Very highly recommended.`,
+source: '1',
+imageUrl: 'https://lh3.googleusercontent.com/a-/AOh14GjNglKO9wAbgJqg3F6Bfbh7w2Iboeu66rKBBfUUyg'
 }
 ,
 {
 guid: '43d17fd0-059e-42e2-bb40-4166b886c727',
-name: 'Majellan',
-review: `I love no mans sky, but I’m pretty add so I have spent literal hours in no mans sky forgetting mats and running around in circles collecting mats inefficiently.  Then of course, squirrel and all of my plans disappear into the ether.  This app an outstanding tracker, very glad that I came across it.  Helps me to track what I’d like to do next and what mats I’ll need for each of my bases-ships-freighters etc.  Took me a bit to optimize organization for my purposes but very worth it.    Now I can’t play the game without it, instead of searching all the time, I am actually playing while in game. `,
-source: '2',
-imageUrl: 'https://nmsassistant.com/assets/img/AppStoreIcon.png'
+name: 'Mark Bailey',
+review: `A lot of thought and hard work has gone into making this superb app. Everything that I need to know is all in one place rather than going on Wiki every time I need information. I love the search option too which saves scrolling down. An easy five stars from me. Thank you!`,
+source: '1',
+imageUrl: 'https://lh3.googleusercontent.com/a-/AOh14Gj-ZWH8rLMGmsdSWH5doac8ljPhJDrgSCNPW7rDvw'
 }
 ,
 {
 guid: '54cd2499-fa5e-475e-bc52-9bd56227d12a',
-name: 'Jeremiah von Walsung',
-review: `This is excellent and Dev seems to stay on top of updates. I recommend this app to all members of the NMS Galactic Empire, NMS Cafe and to players in the NMS Amino. Thank you for keeping this up to date and look forward to your future work on this app (more updates to NMS in 2020). I am Grand Moff Willhuff Tarkin (NMS Galactic Empire)nand I was not compensated for this review.`,
+name: 'Messenger Project',
+review: `This is hands down the best assistant game info app i have ever downloaded, the amount of effort put in is amazing and and the attention to feedback is excellent if i could give it 6 stars id give 10, I know this is a bit winded but i have never seen anyone put so mutch effort into an app like this.`,
 source: '1',
-imageUrl: 'https://lh3.googleusercontent.com/a-/AAuE7mDzciYRQxlcbBQnqw8IpX7GLWE3Dk4VdT61SkTQq3o=s48-c'
+imageUrl: 'https://lh3.googleusercontent.com/a-/AOh14GiJK9k1ZeG5N64_zXKgZsA6OWjL9xu-oXR7Vad7sw'
 }
 ,
 {
 guid: '56888759-d47a-42f9-81d2-2a6b104b5328',
-name: 'Zeedge !',
-review: `You can find just about EVERYTHING you need and MORE for No Mans Sky! This App is SO amazing! I use this app every single time I play No Mans Sky and I play nms all the time! GREAT JOB! Looking forward to future updates! Will reccomend to my buddies!`,
+name: 'Vidhu Nath',
+review: `Fantastic and very helpful app! Maybe the only thing I could see as further expansion is including a frigate quality calculator or some other tools that might exist (the integration with the base power calculator is great!), but even that isn't really an issue since the quality of the app is superb.`,
 source: '1',
-imageUrl: 'https://lh3.googleusercontent.com/a-/AAuE7mCneV3wagVJppeggRL0FjkGpFYtAArlIg6ZMkPYyQ=s48-c'
+imageUrl: 'https://lh3.googleusercontent.com/a/AATXAJy8BoMvPdMAZ7yCLl_zCvtgCzcDjlwmpWRNIOrw=mo'
 }
 ,
 {
 guid: '9eae3a79-a9a3-4847-9a2b-2c9598f7bbf1',
-name: 'Marcus Freiberger',
-review: `The app is simply amazing, someone really put a lot of effort into it. I put my thumbs up and pull all of my hut. Great job, honestly. 👍🏿👍🏿👍🏿`,
+name: 'Adam Davis',
+review: `I recently downloaded No Mans Sky and I'd have to admit, with this app it keeps me from having to write everything down on notepad. I'm very fortunate to have this as a tool.`,
 source: '1',
-imageUrl: 'https://lh3.googleusercontent.com/-9vv_pkybXHE/AAAAAAAAAAI/AAAAAAAAAAA/ACHi3reb_KGBaEGVXzIubd6nr15fB1ZR3A/s48-c/photo.jpg'
+imageUrl: 'https://lh3.googleusercontent.com/a-/AOh14GhYCl8VhffUtapqnS1uqQgpIWko9NC0Iya5PyHoCw'
 }
 ,
 {
 guid: 'be51840c-02d9-4454-b5f6-46bedc96510f',
-name: 'gbWordBoy',
-review: `Compared ‘em all, and this app is easily the most thorough and integrated utility app in a fast, clean interface. For example, cactus flesh entry has full game summary, used to create, refined with, refine to create and cooking recipes all on one page for quick inspection - and every item is clickable to cross reference to that section. A visual database, tightly wired together, very nice!`,
-source: '2',
-imageUrl: 'https://nmsassistant.com/assets/img/AppStoreIcon.png'
+name: 'PoisonusPriest',
+review: `Even when i lost my original save on nms this app helped me craft the food products to keep me alive until i had a suitable base constructed. Keep up the great worm with this app!!!`,
+source: '1',
+imageUrl: 'https://lh3.googleusercontent.com/a-/AOh14GguKCuY4AaRAl7Cnopzg61nhJhHEw9Pb95h_OeJ-g'
 }
 ,
 {
 guid: '9aa94ae3-8292-4883-9412-78e2178858e5',
-name: 'dalkulin daglow',
-review: `Absolutely wonderful and Incredibly so if you are an avid player of No Man's sky or even the Casual I cannot recommend having this app enough it is saved me several hours of searching and exploring as well as a general aid for finding objects that are not listed in most other I guess to be assumed assist sites in other words I can't recommend this enough it's very good it looks very fast and very helpful Five Stars perfect`,
+name: 'Nømad Natiøn',
+review: `Hate to use you're comment section to find people to run with or hey maybe you'd even run with me and build some settlements with me lol but seriously great app man it's helpful. But the discord chat you have linked to the app isn't very active is there any chance you could find another chat that stays active if not that's fine I know it can be hard`,
 source: '1',
-imageUrl: 'https://lh3.googleusercontent.com/a-/AAuE7mD-PEiRS3J6wBV9gZ3pwb8j9VX4WLE5pasNbcUIZA=s48-c'
+imageUrl: 'https://lh3.googleusercontent.com/a-/AOh14GiAvWDt6x0LZwU3KPvo8qbbnslvUifHNC2nN5Df'
 }
 ,
 {
 guid: '4be9aef3-4654-4b9c-a1d8-acb92c5e4589',
-name: 'Alfred Hamilton',
-review: `A very handy tool, prevents me from opening screens on my pc`,
+name: 'Tadas Jankauskas',
+review: `It would be nice if there was shown how much frigate modules are needed for freighter technologies and also way to mark technologies/blueprints in technology tree that has been learned ( why is this app marked as having ads? I don't see any ads)`,
 source: '1',
-imageUrl: 'https://lh3.googleusercontent.com/a-/AAuE7mDhwULn_yj_g62sIrDX0Tas-kTlrCP2_uqMfa_X20c=s48-c'
+imageUrl: 'https://lh3.googleusercontent.com/a/AATXAJxeBtEOKGEU_W4U6mIDgUXA_Sr8S45QX-ZHLmnJ=mo'
 }
 ,
 {
 guid: '060d9c76-b25d-469d-b5a9-01793b39f6c8',
-name: 'Fresh Like',
-review: `Richtig gute app und top aktuell synchron mit dem Spiel No Man's Sky 👍👍👍👍 Nice App`,
+name: 'Dan Skelton',
+review: `Absolutely incredible app! Been playing NMS on PC/Steam for sometime now, and still need to refer back to Wiki's and what not. Found this app, and it's been super incredibly helpful, and the fact content is not locked behind a paywall is even better. I'd highly consider donating just for that. Seriously worth getting. Thank you so much!`,
 source: '1',
-imageUrl: 'https://lh5.googleusercontent.com/-ei343cmMaIc/AAAAAAAAAAI/AAAAAAAAAAA/ACHi3rfkAzI4vu2DFwUKN1ooo8ETeNFWkQ/s48-c/photo.jpg'
+imageUrl: 'https://lh3.googleusercontent.com/a-/AOh14GhjTqEWRM1CNWHGjdK5RWAgetqBScd_Cs0UwejK6Q'
 }
 ,
 {
 guid: '1206121e-3739-47a5-a088-5d631bf90726',
-name: 'Angelus Clauford',
-review: `Thanks for Polish translation. Your app is very helpful in game. Good work 🙂`,
+name: 'JOYCEY',
+review: `An incredibly well thought out and put together gaming app. It's amazing to see how someone who clearly has passion about nms can make an app so well`,
 source: '1',
-imageUrl: 'https://lh3.googleusercontent.com/a-/AAuE7mCkm3E9MCrLqwkccOh42IICaP_gA8g7oM5az2sDJA=s48-c'
+imageUrl: 'https://lh3.googleusercontent.com/a-/AOh14GgPduu74hOvqhrjUGzrqEv-bumr2PmbtPKNI3HKBg'
 }
 ,
 {
 guid: '452cfa1c-7afc-460e-8eb2-9076b1391526',
-name: 'Hackelsepp',
-review: `I am speechless and that has to mean something, good work with the app it helps me a lot and I can only recommend it for newcomers and veterans who play No man's sky. It is helpful and easy to use and since I do not know every recipe by heart, it has helped me with some things. Keep it up 👍`,
+name: 'Zombemi',
+review: `This is great, it's so useful and the UI is so tidy, nicely made and quite intuitive. It's saved me quite a lot of time, it's a lot quicker than just googling everything or getting increasingly frustrated as you try to find it on your own. (I sought this out when I found myself cursing a planet out.) Though one feature I'd love to have is just a note added about whether or not we need to raid a facility for a blueprint or something like that.`,
 source: '1',
-imageUrl: 'https://lh3.googleusercontent.com/a-/AAuE7mA8VYLp0YlsLyg2hc_Tulorm1bOuEqYEAcN4t7b=s48-c'
+imageUrl: 'https://lh3.googleusercontent.com/a-/AOh14Gh0RuKHn_ERxsmYRM2w5yMYUW8EZwyjjc8dwgwx6w'
 }
 ,
 {
-guid: '7872882b-c898-4df7-9976-9e1a069f26a1',
-name: 'Hackelsepp',
-review: `Ich bin Sprachlos und das muss schon was heißen, Gute Arbeit mit der App sie hilft mir sehr und ich kann dass nur empfehlen für neueinsteiger und den Veteranen die No man's sky spielen. Sie ist hilfreich und einfach zu bedienen und da ich nicht jedes Rezept auswendig weiß hat sie mir bei manchen Sachen Geholfen. Mach weiter so 👍`,
+guid: '1dbed229-5969-45a4-9777-21b5cd14189c',
+name: 'Cyber 4Eyes',
+review: `An outstanding app with tons of information related to No Man's Sky. Keeping this handy on a mobile device while playing the game is an immense help. The app also has a record of expeditions, recipes, tips, and more. I can never remember all the recipes. This app saves you time in looking up the needed info. If you play No Man's Sky, get this app! Heck, get it even if you are curious about the game and check out all it's features.`,
 source: '1',
-imageUrl: 'https://lh3.googleusercontent.com/a-/AAuE7mA8VYLp0YlsLyg2hc_Tulorm1bOuEqYEAcN4t7b=s48-c'
+imageUrl: 'https://lh3.googleusercontent.com/a-/AOh14GicCa8r555IEsDn_wB0-QorAdQLp5cGDGtaU3FvtA'
 }
 ];
 
