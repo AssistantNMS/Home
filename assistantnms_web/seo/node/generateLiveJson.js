5bc4e3a0c6877be1845e38c43acac15366b28176
Kurt Lourens <hi@kurtlourens.com>
2022-06-29 13:37:07 +0200

    fix links

--- assistantnms_web/seo/node/generateLiveJson.js
+++ assistantnms_web/seo/node/generateLiveJson.js
@@ -4,15 +4,21 @@ const axios = require('axios');
 async function generateLiveJson() {
 
     const appReviewJson = await axios.get('https://api.assistantapps.com/AppReview/1');
+    console.log('appReviewJson');
 
     const contributorsJson = await axios.get('https://api.nmsassistant.com/Contributor');
+    console.log('contributorsJson');
 
     const testimonialsJson = await axios.get('https://api.nmsassistant.com/Testimonial');
+    console.log('testimonialsJson');
 
     const donationJson = await axios.get('https://api.assistantapps.com/Donation?page=1');
+    console.log('donationPageJson 1');
     let donationList = [...donationJson.data.value];
     for (let donationIndex = 1; donationIndex < donationJson.data.totalPages; donationIndex++) {
-        const donationPageJson = await axios.get(`https://api.assistantapps.com/Donation?page=${donationIndex + 1}`);
+        const pageToRequest = donationIndex + 1;
+        const donationPageJson = await axios.get(`https://api.assistantapps.com/Donation?page=${pageToRequest}`);
+        console.log('donationPageJson ' + pageToRequest);
         donationList = [...donationList, ...(donationPageJson?.data?.value ?? [])];
     }
 
