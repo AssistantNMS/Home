98a2e186e68a11650cde95f4c043c7805740743d
Khaoz-Topsy <hi@kurtlourens.com>
2022-07-01 09:51:54 +0000

    Deploying to gh-pages from @ AssistantNMS/Home@91d8d40625aa378d74cf28c443a05a522d2cf742 🚀

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -6,10 +6,38 @@ function heroParallax() {
     }
 }
 
+function featureParallax() {
+    var featureParallaxElems = document.getElementsByClassName('feature-parallax');
+    var featureParallaxInstances = [];
+    for (const featureParallaxElem of featureParallaxElems) {
+        featureParallaxInstances.push(new Parallax(featureParallaxElem));
+    }
+}
+
+const registerServiceWorker = async () => {
+    if ('serviceWorker' in navigator) {
+        try {
+            const registration = await navigator.serviceWorker.register('/serviceWorker.js');
+            if (registration.installing) {
+                console.log('Service worker installing');
+            } else if (registration.waiting) {
+                console.log('Service worker installed');
+            } else if (registration.active) {
+                console.log('Service worker active');
+            }
+        } catch (error) {
+            console.error(`Registration failed with ${error}`);
+        }
+    }
+};
+
 function customOnLoad() {
     try {
-        var s = skrollr.init(); // global skrollr instance
-        heroParallax();
+        if ((screen?.width ?? 0) > 1000) {
+            var s = skrollr.init(); // global skrollr instance
+            heroParallax();
+            featureParallax();
+        }
     } catch { }
 
     try {
@@ -24,3 +52,4 @@ setTimeout(() => {
     customOnLoad?.();
 }, 1000);
 
+registerServiceWorker();
