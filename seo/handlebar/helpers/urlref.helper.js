981e6e0d902a8ee9694bf3325a8c753fbd111d1c
Khaoz-Topsy <hi@kurtlourens.com>
2022-07-05 17:04:33 +0000

    Deploying to gh-pages from @ AssistantNMS/Home@168be4fd17876c6e91bcaea2766332692e937706 🚀

--- seo/handlebar/helpers/urlref.helper.js
+++ seo/handlebar/helpers/urlref.helper.js
@@ -1,4 +1,5 @@
 module.exports = function (context, options) {
+    // if (context == null) return 'error';
     if (context.includes('?')) {
         return context + '&ref=assistantNMS';
     }
--- serviceWorker.js
+++ serviceWorker.js
@@ -1,4 +1,4 @@
-const PRECACHE = 'precache-v4.0.14';
+const PRECACHE = 'precache-v4.0.15';
 const RUNTIME = 'runtime';
 
 // A list of local resources we always want to be cached.
